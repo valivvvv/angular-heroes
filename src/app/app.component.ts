756f9dfc035884fa147e8daa1910be979bb41e44
vali <vali@royalosft.eu>
2017-05-19 18:13:09 +0300

    https://angular.io/docs/ts/latest/tutorial/toh-pt5.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+    <h1>{{title}}</h1>
+    <my-heroes></my-heroes>
+  `
 })
-export class AppComponent  { name = 'Angular'; }
+
+export class AppComponent {
+  title = 'Tour of Heroes';
+}
